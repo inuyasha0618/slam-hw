6b3c457423b69efba2a4dc764a40771a85ab0b39
inuyasha0618 <chenjiandiwang@qq.com>
2018-01-30 10:45:12 +0800

    remove useless

--- homework6/optical_flow.cpp
+++ homework6/optical_flow.cpp
@@ -177,14 +177,11 @@ void OpticalFlowSingleLevel(
                 for (int y = -half_patch_size; y < half_patch_size; y++) {
 
                     // TODO START YOUR CODE HERE (~8 lines)
-//                    double error = img1.at<uchar>(kp.pt.y, kp.pt.x) - img2.at<uchar>(kp.pt.y + dy, kp.pt.x + dx);
                     double error = GetPixelValue(img1, kp.pt.x + x, kp.pt.y + y) - GetPixelValue(img2, kp.pt.x + x + dx, kp.pt.y + y + dy);
                     Eigen::Vector2d J;  // Jacobian
                     if (inverse == false) {
                         // Forward Jacobian
-//                        J(0) = -(img2.at<uchar>(kp.pt.y + dy, kp.pt.x + dx + 1) - img2.at<uchar>(kp.pt.y + dy, kp.pt.x + dx - 1))/ 2;
                         J(0) = -(GetPixelValue(img2, kp.pt.x + x + dx + 1, kp.pt.y + y + dy) - GetPixelValue(img2, kp.pt.x + x + dx - 1, kp.pt.y + y + dy)) /2;
-//                        J(1) = -(img2.at<uchar>(kp.pt.y + dy + 1,  kp.pt.x + dx) - img2.at<uchar>(kp.pt.y + dy - 1,  kp.pt.x + dx))/ 2;
                         J(1) = -(GetPixelValue(img2, kp.pt.x + x + dx, kp.pt.y + y + dy + 1) - GetPixelValue(img2, kp.pt.x + x + dx, kp.pt.y + y + dy - 1)) /2;
                     } else {
                         // Inverse Jacobian
